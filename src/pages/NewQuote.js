fd8a9b1d2e8a03824151cfcea0417f79fe0e0993
Aylin Sayharman <aylin.sayharman@gmail.com>
2021-12-09 00:13:35 +0300

    add dummy data

--- src/pages/NewQuote.js
+++ src/pages/NewQuote.js
@@ -1,5 +1,10 @@
+import QuoteForm from '../components/quotes/QuoteForm';
+
 const NewQuote = () => {
-  return <h1>New quote</h1>
+  const addQuoteHandler = (data) => {
+    console.log(data)
+  }
+  return <QuoteForm onAddQuote={addQuoteHandler}></QuoteForm>
 };
 
 export default NewQuote;
