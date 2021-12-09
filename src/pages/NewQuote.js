6e20c4fcf421f23bf59b387eddbbd97fa98f2dd0
Aylin Sayharman <aylin.sayharman@gmail.com>
2021-12-09 21:25:36 +0300

    add useHistory

--- src/pages/NewQuote.js
+++ src/pages/NewQuote.js
@@ -1,10 +1,16 @@
 import QuoteForm from '../components/quotes/QuoteForm';
+import { useHistory } from 'react-router-dom';
 
 const NewQuote = () => {
+
+  const history = useHistory();
+
   const addQuoteHandler = (data) => {
     console.log(data)
+
+    history.push('/quotes');
   };
-  return <QuoteForm onAddQuote={addQuoteHandler}></QuoteForm>
+  return <QuoteForm onAddQuote={addQuoteHandler}/>
 };
 
 export default NewQuote;
